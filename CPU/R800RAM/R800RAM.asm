929c75acd2a7c009a3c4b22a0b25d7750283f325
peterlemon <krom@gbadev.org>
2019-12-16 10:01:22 +0000

    R800 RDRAM Mode Test

---
+++ CPU/R800RAM/R800RAM.asm
@@ -0,0 +1,32 @@
+// MSX Turbo-R CPU R800 RAM Mode demo by krom (Peter Lemon):
+arch msxtr.cpu
+output "R800RAM.rom", create
+fill $8000 // Set ROM Size (32KB)
+
+origin $0000
+base $4000 // Entry Point Of Code
+include "LIB/MSX_HEADER.ASM" // Include MSX ROM Header
+include "LIB/MSX_SYSTEM.INC" // Include MSX System Routines
+
+Start:
+  ld hl,CHMSG // HL = Character Message Address
+
+PrintLoop:
+  ld a,(hl) // A = Character
+  or a // A |= A
+  jp z,CPUMode // IF (Character == Zero) Finish Printing
+  call CHPUT // CALL System Routine To Display A Character To The Screen
+  inc hl // Character Message Address++
+  jr PrintLoop // Continue Printing
+
+CPUMode: // Change CPU Mode To R800 ROM Mode
+  ld a,%10000010 // A = Settings: %L00000MM - L = LED (1 = Switched On With CPU Mode), M = CPU Mode (2 = R800 DRAM Mode)
+  call CHGCPU // CALL System Routine To Change CPU Mode
+
+Loop:
+  jr Loop
+
+CHMSG:
+  db 13,"\nMSX Turbo-R CPU R800 RAM Mode\n\n" // Text (Null-Terminated String)
+  db 13,"CPU Mode Set To R800 RAM Mode\n\n"
+  db 13,"TURBO LED Switched On",0
